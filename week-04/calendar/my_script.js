d136447a13973a8053ebbd37a16a5209436bd3bd
Dariusz Franczak <darius32@op.pl>
2016-03-14 00:20:09 +0000

    still needs mediaqueries to be fully responsive

--- week-04/calendar/my_script.js
+++ week-04/calendar/my_script.js
@@ -9,35 +9,44 @@ title.className = "title";
 wrapper.appendChild(title);
 title.innerHTML = '<h1>March <span>2016</span></h1>'
 
+var container1 = document.createElement('div');
+container1.className = "flex-container";
+wrapper.appendChild(container1);
+
 var names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 
 
 for(i=0; i<7; i++) {
     var dayNameBox = document.createElement('div');
+    var p = document.createElement('p');
     dayNameBox.className = "daynames";
-    dayNameBox.innerHTML = '<p></p>';
-    wrapper.appendChild(dayNameBox);
+    dayNameBox.appendChild(p);
+    container1.appendChild(dayNameBox);
 }
 
 
 
 for(i=0; i<names.length; i++) {
-	document.getElementsByClassName('daynames')[i].textContent = names[i];
+    document.getElementsByClassName('daynames')[i].innerHTML = names[i];
 }
 
+var container2 = document.createElement('div');
+container2.className = "flex-container";
+wrapper.appendChild(container2);
 
 for(x=0; x<42; x++) {
     var dayBox = document.createElement('div');
     dayBox.className = "days";
     dayBox.innerHTML = '<p></p>';
-    wrapper.appendChild(dayBox);
+    container2.appendChild(dayBox);
 }
 
 
 
 for(var i=1; i<32; i++){
 
-	document.getElementsByClassName('days')[i].textContent = i;
+    document.getElementsByClassName('days')[i].textContent = i;
 }
 
 var today = new Date().getDate();
@@ -45,12 +54,15 @@ todayStr = today.toString();
 
 
 for(var x=0; x<document.getElementsByClassName('days').length; x++){
-	if (document.getElementsByClassName('days')[x].textContent === todayStr){
-		document.getElementsByClassName('days')[x].className = "border";
-		
-	}
-}
-
-
+    if (document.getElementsByClassName('days')[x].textContent === todayStr){
+        document.getElementsByClassName('days')[x].className = "border";
+        
+    }
 };
+
+
+
+
+
+}
 
