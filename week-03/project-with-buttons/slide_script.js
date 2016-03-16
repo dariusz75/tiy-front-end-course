3451cbe645b5d6b247b5d51eb72b404e338d5ab0
Dariusz Franczak <darius32@op.pl>
2016-03-16 22:41:37 +0000

    layout created, gallery not working

--- week-03/project-with-buttons/slide_script.js
+++ week-03/project-with-buttons/slide_script.js
@@ -1,61 +1,31 @@
 $(document).ready(function () {
 
 
-	// creating wrapper
-  var wrapper = document.createElement('div');
-  wrapper.setAttribute('id', 'wrapper');
-  document.body.appendChild(wrapper); 
+// ################# Wrapper and buttons ################### //
 
-  var buttonLeft = document.createElement('button');
-  buttonLeft.setAttribute('id', 'left');
-  wrapper.appendChild(buttonLeft);
+var wrapper = $('<div>').attr('id','wrapper').appendTo(document.body);
+var buttonLeft = $('<button>').attr('id','left').appendTo(wrapper);
+var buttonRight = $('<button>').attr('id','right').appendTo(wrapper);
 
-  var buttonRight = document.createElement('button');
-  buttonRight.setAttribute('id', 'right');
-  wrapper.appendChild(buttonRight);
+// ################# Main picture ################### //
 
-  // ################# Slideshow ################### //
-
-  // creating main picture div
-  var mainPicture = document.createElement('div');
-  mainPicture.setAttribute('id', 'mainPicture');
-  wrapper.appendChild(mainPicture); 
-
-  var pict1 = document.createElement('img');
-  pict1.setAttribute('src', 'images/image-01.jpg');
-  mainPicture.appendChild(pict1); 
+var mainPicture = $('<div>').attr('id', 'mainPicture').appendTo(wrapper);
+var pict1 = $('<img>').attr('src', 'images/image-01.jpg').appendTo(mainPicture);
 
 
-  console.log(pict1.hesAttribute = 'src');
+ // ################## THUMBNAILS ################################
+ 
+var thumbnail1 = $('<div>').attr('class', 'thumbnail-border').appendTo(wrapper);
+var image1 = $('<img>').attr('src', 'images/image-01.jpg').appendTo(thumbnail1);
 
-  // ################## THUMBNAILS ################################
-  var thumbnail1 = document.createElement('div');
-  thumbnail1.setAttribute('class', 'thumbnail-border');
-  wrapper.appendChild(thumbnail1); 
-  var image1 = document.createElement('img');
-  image1.setAttribute('src', 'images/image-01.jpg');
-  thumbnail1.appendChild(image1);
+var thumbnail2 = $('<div>').attr('class', 'thumbnail').appendTo(wrapper);
+var image2 = $('<img>').attr('src', 'images/image-02.jpg').appendTo(thumbnail2);
 
-  var thumbnail2 = document.createElement('div');
-  thumbnail2.setAttribute('class', 'thumbnail');
-  wrapper.appendChild(thumbnail2); 
-  var image2 = document.createElement('img');
-  image2.setAttribute('src', 'images/image-02.jpg');
-  thumbnail2.appendChild(image2);
+var thumbnail3 = $('<div>').attr('class', 'thumbnail').appendTo(wrapper);
+var image3 = $('<img>').attr('src', 'images/image-03.jpg').appendTo(thumbnail3);
 
-  var thumbnail3 = document.createElement('div');
-  thumbnail3.setAttribute('class', 'thumbnail');
-  wrapper.appendChild(thumbnail3); 
-  var image3 = document.createElement('img');
-  image3.setAttribute('src', 'images/image-03.jpg');
-  thumbnail3.appendChild(image3);
-
-  var thumbnail4 = document.createElement('div');
-  thumbnail4.setAttribute('class', 'thumbnail');
-  wrapper.appendChild(thumbnail4); 
-  var image4 = document.createElement('img');
-  image4.setAttribute('src', 'images/image-04.jpg');
-  thumbnail4.appendChild(image4);
+var thumbnail4 = $('<div>').attr('class', 'thumbnail').appendTo(wrapper);
+var image4 = $('<img>').attr('src', 'images/image-04.jpg').appendTo(thumbnail4);
 
   var thumbnails = [
     thumbnail1,
@@ -65,9 +35,6 @@ $(document).ready(function () {
   ]
 
  
-
-  
-
   var galery = [ 
     'images/image-01.jpg',               
     'images/image-02.jpg',
