554b49c5304ffe7983971400f79fe0aaf369e230
Dariusz Franczak <darius32@op.pl>
2016-03-27 17:54:24 +0100

    jQuery solution added, still needs some more features to be added

--- week-04/calendar/my_script.js
+++ week-04/calendar/my_script.js
@@ -1,86 +1,68 @@
-window.onload = function() {
+$(document).ready(function () {
 
-var wrapper = document.createElement('div');
-wrapper.className = "wrapper";
-document.querySelector('body').appendChild(wrapper);
+     var $wrapper = $("<div></div>");
+     $wrapper.addClass("wrapper");
+     $wrapper.appendTo(document.body);
 
-var title = document.createElement('div');
-title.className = "title";
-wrapper.appendChild(title);
-title.innerHTML = '<h1>March <span>2016</span></h1>'
+     var $title = $("<div><h1>March<span> 2016</span></h1></div>");
+     $title.addClass("title");
+     $title.appendTo($wrapper);
 
-var container1 = document.createElement('div');
-container1.className = "flex-container";
-wrapper.appendChild(container1);
-
-var names = ['<p>Monday</p>', '<p>Tuesday</p>', '<p>Wednesday</p>', '<p>Thursday</p>', '<p>Friday</p>', '<p>Saturday</p>', '<p>Sunday</p>'];
+     var $container1 = $("<div></div>");
+     $container1.addClass("flex-container");
+     $container1.appendTo($wrapper);
 
 
+(function insertDaynames(){
+        
+        var names = ['<p>Monday</p>', '<p>Tuesday</p>', '<p>Wednesday</p>', '<p>Thursday</p>', '<p>Friday</p>', '<p>Saturday</p>', '<p>Sunday</p>'];
 
-for(i=0; i<7; i++) {
-    var dayNameBox = document.createElement('div');
-    dayNameBox.className = "daynames";
-    container1.appendChild(dayNameBox);
-}
-
-
-
-for(i=0; i<names.length; i++) {
-    document.getElementsByClassName('daynames')[i].innerHTML = names[i];
-}
-
-var container2 = document.createElement('div');
-container2.className = "flex-container";
-wrapper.appendChild(container2);
-
-for(x=0; x<42; x++) {
-    var dayBox = document.createElement('div');
-    dayBox.className = "days";
-    dayBox.innerHTML = '<p></p>';
-    container2.appendChild(dayBox);
-
-
-    dayBox.onclick=function(){    
-    this.className='days-highlight'; 
-    console.log('test-ok');
-    }
+        for(var i=0; i<names.length; i++)
+        {
+            $( "<div class='daynames'>" + names[i] + "</div>" ).appendTo($container1);
+        }
     
+})();  
 
-    dayBox.addEventListener("click", modalWindow, false);
+var $container2 = $("<div></div>");
+     $container2.addClass("flex-container");
+     $container2.appendTo($wrapper);
 
-    function modalWindow() {
-    var popup = document.createElement('div');
-    popup.innerHTML = '<div class="modal"><form><textarea  name="textarea" rows="10" cols="50">Write something here</textarea><button id="submit">Submit</button><button id="reset">Reset</button></form></div>'
-    document.querySelector('body').appendChild(popup);
-    }
+
+(function insertDayboxes(){
+                
+        for(var i=0; i<42; i++)
+        {
+            $( "<div class='days'><p></p></div>" ).appendTo($container2);
+        }
     
-    
-
-
-
-}
-
+})();
 
 
 for(var i=1; i<32; i++){
 
-    document.getElementsByClassName('days')[i].innerHTML = '<p>'+i+'</p>';
-}
+      document.getElementsByClassName('days')[i].innerHTML = '<p>'+i+'</p>';
+  }
 
 var today = new Date().getDate();
-todayStr = today.toString();
+  todayStr = today.toString();
 
 
-for(var x=0; x<document.getElementsByClassName('days').length; x++){
-    if (document.getElementsByClassName('days')[x].textContent === todayStr){
-        document.getElementsByClassName('days')[x].className = "border";
-        
+  for(var x=0; x<document.getElementsByClassName('days').length; x++){
+      if (document.getElementsByClassName('days')[x].textContent === todayStr){
+          document.getElementsByClassName('days')[x].className = "border";
+          
+      }
     }
-};
+
+
+$(".days").on('click', function() {
+    $(this).css('background-color', '#FF4E42');
+    $('<div class="modal"><form><textarea  name="textarea" rows="10" cols="50">Write something here</textarea><button class="submit" value="Submit">Submit</button><button type="reset" value="Reset">Reset</button></form></div>').appendTo($wrapper);
+
+
+});
 
 
 
-
-
-}
-
+});
