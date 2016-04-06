0aa93017d16b9a374fe81007f139c1e128df6c0b
Dariusz Franczak <darius32@op.pl>
2016-04-06 21:28:44 +0100

    thumbnails have a red border when clicked

--- week-07/day-03/myscript.jsx
+++ week-07/day-03/myscript.jsx
@@ -1,14 +1,49 @@
-/* 
-var mainPicture = React.createElement('div', { className: 'main-picture', key: 'mainPicture' }, 'Main Picture');
-var thumbnail1 = React.createElement('div', {className: 'thumbnail-1', key: 'thumbnail1'}, 'Thumbnail 1');
-var thumbnail2 = React.createElement('div', {className: 'thumbnail-2', key: 'thumbnail2'}, 'Thumbnail 2');
-var thumbnail3 = React.createElement('div', {className: 'thumbnail-3', key: 'thumbnail3'}, 'Thumbnail 3');
-var thumbnail4 = React.createElement('div', {className: 'thumbnail-4', key: 'thumbnail4'}, 'Thumbnail 4');
-var reactFragment = [ mainPicture, thumbnail1, thumbnail2, thumbnail3, thumbnail4 ];
-var section = React.createElement('section', { className: 'container' }, reactFragment);
-var parentElement = document.querySelector('[data-react-app]');
-ReactDOM.render(section, parentElement);
-*/
 
 
 	
+
+	
+var Thumbnail = React.createClass({
+	getInitialState: function () {
+		return {
+			isSelected: false
+		};
+	}, 
+	handleClick: function () {
+		this.props.handleClick(this.props.counter);
+		this.setState({
+			isSelected: !this.state.isSelected
+		});
+	},
+	render: function () {
+		
+		return <div className={'thumbnail' + (this.state.isSelected ? ' selected' : '')} onClick={this.handleClick}>{'Picture ' + this.props.counter}</div>;				
+	} 
+});
+
+
+
+	var Boxes = React.createClass({
+  getInitialState: function () {
+  	return {
+  		pictureCount: 1
+  	};
+  },
+  changePicture: function (pictureCount){
+  	this.setState({
+  		pictureCount: pictureCount
+  	});
+  },
+  render: function () {
+    return 	<section className="container">
+		    			<div className="main-picture">{this.state.pictureCount}</div>
+		      		<Thumbnail counter="1" handleClick={this.changePicture} />
+		      		<Thumbnail counter="2" handleClick={this.changePicture} />
+		      		<Thumbnail counter="3" handleClick={this.changePicture} />
+      			</section>;  
+  }
+});
+
+
+var parentElement = document.querySelector('[data-react-app]');
+var reactComponent = ReactDOM.render(<Boxes />, parentElement);
