33d2cbbfeac61d6b3776433bc86b7064ed4ff2c0
Dariusz Franczak <darius32@op.pl>
2016-04-12 19:01:09 +0100

    exporting modules as an object

--- week-08/day-02/app.js
+++ week-08/day-02/app.js
@@ -1,3 +1,6 @@
 var welcome = require('./functions.js');
+var info = require('./info.js');
 
 welcome();
+info.infoPolish();
+info.infoEnglish();
---
+++ week-08/day-02/info.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+	infoPolish: function() {
+		console.log('Witamy w YI');
+	},
+
+	infoEnglish: function() {
+		console.log('Welcome to YI');
+	}
+
+}
+
